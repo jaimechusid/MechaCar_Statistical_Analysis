c5488b0dddcf0421d1564d1f65b5d2413a7535ae
jaimechusid <jaimech@umich.edu>
2021-01-22 15:27:12 -0500

    deliverable1

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,4 +1,5 @@
 library(dplyr)
 cars_df <- read.csv('MechaCar_mpg.csv')
-lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=cars_df)
-summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=cars_df))
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=cars_df)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=cars_df))
+
