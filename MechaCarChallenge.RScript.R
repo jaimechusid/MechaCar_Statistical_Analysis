7994a67cb2c17f049336240fa4387b95044012fa
jaimechusid <jaimech@umich.edu>
2021-01-23 17:32:01 -0500

    deliverable2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -3,3 +3,5 @@ cars_df <- read.csv('MechaCar_mpg.csv')
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=cars_df)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=cars_df))
 
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
